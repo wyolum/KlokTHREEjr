945b9963a3808ad1e29ba1bec03f0c9509e9010f
Anool <anool.m@gmail.com>
2018-01-23 18:09:00 +0530

    added _anool enclosure in fabricate

--- arduino/libraries/credentials/credentials.h
+++ arduino/libraries/credentials/credentials.h
@@ -3,14 +3,14 @@
 // ---- credentials.h ----
 #ifndef __CREDENTIALS_H
 #define __CREDENTIALS_H
-char ssid[] = "XXXXXXXXXX";  // your network SSID (name)
-char pass[] = "YYYYYYYYYY";  // your network password
+char ssid[] = "Lumetronics";  // your network SSID (name)
+char pass[] = "n0rtemul";  // your network password
 #define WIFI_SSID ssid
 #define WIFI_PASS pass
 /************************* Adafruit.io Setup *********************************/
 #define IO_SERVER      "io.adafruit.com"
 #define IO_SERVERPORT  8883
-#define IO_USERNAME  "YOUR_USER_NAME"
-#define IO_KEY       "YOUR_IO_SECRET_KEY"
+#define IO_USERNAME  "Anool"
+#define IO_KEY       "963e5d6265db4390b8a0b95bd5deecd1"
 
 #endif
