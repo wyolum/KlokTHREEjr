53838fb571a60f2e89f59b3013db28d4e680bf70
anool <anool.m@gmail.com>
2018-01-29 23:16:38 +0530

    Anool, added missing language files to arduino/KlokTHREEjr_v2

---
+++ arduino/KlokTHREEjr_v2/config.h
@@ -0,0 +1,15 @@
+#include <credentials.h>
+
+// comment out the following two lines if you are using fona or ethernet
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, ssid, pass);
+
+/************************* IO Setup *********************************/
+//WiFiClientSecure client;
+//Adafruit_MQTT_Client mqtt(&client, IO_SERVER, IO_SERVERPORT, IO_USERNAME, IO_USERNAME, IO_KEY);
+//Adafruit_MQTT_Subscribe time_seconds = Adafruit_MQTT_Subscribe(&mqtt, "time/seconds"); // used NTPClient instead
+AdafruitIO_Feed *timezone = io.feed("Timezone1");
+AdafruitIO_Feed *brightness = io.feed("klok_brightness");
+
+
+bool FLIP_DISPLAY = true; // true: cord out bottom, false: cord out top
